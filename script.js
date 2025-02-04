cf5241f6aee7b64d47b6df39862093f3a9af33f7
Pondering-Apple <136867740+Pondering-Apple@users.noreply.github.com>
2025-02-04 11:25:12 -0500

    adding javascript for console logging. A step towards creating movement in certain locations

--- script.js
+++ script.js
@@ -1,12 +1,38 @@
-let x = 0;
-let y = 111;
+document.addEventListener("DOMContentLoaded", function () {
+	initializeGrid();
+	trackItems();
+});
 
-while (x <= y) {
-	x = x + 1;
+window.addEventListener("resize", function () {
+	console.log("Window Resized");
+	trackItems();
+});
 
-	let gridBackground = document.getElementById("grid");
-	let gridItem = document.createElement("div");
-	gridItem.className = `grid-item`;
-	gridItem.id = `gridIdentifier${x}`;
-	gridBackground.appendChild(gridItem);
+function initializeGrid() {
+	let x = 0;
+	let y = 111;
+
+	while (x <= y) {
+		x = x + 1;
+
+		let gridBackground = document.getElementById("grid");
+		let gridItem = document.createElement("div");
+		gridItem.className = `grid-item`;
+		gridItem.id = `gridIDNumber${x}`;
+		gridBackground.appendChild(gridItem);
+
+		// document.getElementById(`gridIdentifier${x}`).style.borderBottomColor =
+		// 	"blue";
+	}
+}
+
+//grid items start at multiples of 14
+function trackItems() {
+	let gridItems = document.querySelectorAll(".grid-item");
+	gridItems.forEach((item) => {
+		let rect = item.getBoundingClientRect();
+		console.log(
+			`ID: ${item.id}\nHeight: ${rect.height}\nWidth: ${rect.width}\nTop: ${rect.top}\nLeft: ${rect.left}`
+		);
+	});
 }
